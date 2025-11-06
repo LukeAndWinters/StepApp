c6a712794cc9b54846f2ef41791d5d1cff178374
ACavoli <ahcavoli@gmail.com>
2025-11-06 22:04:26 +1100

    xcode project init

---
+++ Amble/Amble/AmbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmbleApp.swift
+//  Amble
+//
+//  Created by Alessandro on 6/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct AmbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
